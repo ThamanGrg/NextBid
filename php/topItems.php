73b06faabf7e1c3e3305957b4b16729b5b6b0a75
ThamanGrg <gurungthaman246@gmail.com>
2025-01-14 22:51:07 +0545

    top item list

--- php/topItems.php
+++ php/topItems.php
@@ -1,10 +1,22 @@
 <?php
 
 function topItems(){
-    $element ="
-        
-        
+    $element =
+    "
+        <div class=\"topItemsCard\">
+            <div class=\"cardImage\">
+                <div class=\"imageContainer\">
+                    <img src=\"../../../uploads/pexels-mikebirdy-3729464.jpg\" alt=\A title of an Item\">
+                </div>
+            </div>
 
+            <div class=\"itemDetails\">
+                <h2>A title of an Item</h2>
+                <p>Time: 1 DAYS LEFT</p>
+                <p>Current Bid: $00</p>
+            </div>
+            <button class=\"bidButton\">Bid</button>
+        </div>
     ";
     echo $element;
 }
