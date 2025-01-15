18253a52d3614be499193e2b78ba58fabc4c2c78
ThamanGrg <gurungthaman246@gmail.com>
2025-01-15 21:40:37 +0545

    create auction

---
+++ php/connection.php
@@ -0,0 +1,15 @@
+<?php
+
+$servername = "localhost";
+$username = "nextbid";
+$password = "BibekThamanPrachanda";
+$db = "nextbid";
+
+$conn = new mysqli($servername, $username, $password, $db);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} else {
+    echo "Connected successfully";
+}
+?>
