4211287784285c6efc40a695b478af26730d4233
ThamanGrg <gurungthaman246@gmail.com>
2025-01-16 19:09:06 +0545

    create auction form

--- php/connection.php
+++ php/connection.php
@@ -9,7 +9,5 @@ $conn = new mysqli($servername, $username, $password, $db);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
-} else {
-    echo "Connected successfully";
 }
 ?>
--- php/topItems.php
+++ php/topItems.php
@@ -1,22 +1,25 @@
 <?php
 
-function topItems(){
-    $element =
-    "
-        <div class=\"topItemsCard\">
-            <div class=\"cardImage\">
-                <div class=\"imageContainer\">
-                    <img src=\"../../../uploads/pexels-mikebirdy-3729464.jpg\" alt=\A title of an Item\">
+include_once('connection.php');
+
+function topItems($title = "A title of an Item", $timeLeft = "1 DAYS LEFT", $currentBid = "$0", $imageSrc = "../../../uploads/pexels-mikebirdy-3729464.jpg") {
+    $element = <<<HTML
+        <div class="topItemsCard">
+            <div class="cardImage">
+                <div class="imageContainer">
+                    <img src="$imageSrc" alt="$title">
                 </div>
             </div>
 
-            <div class=\"itemDetails\">
-                <h2>A title of an Item</h2>
-                <p>Time: 1 DAYS LEFT</p>
-                <p>Current Bid: $00</p>
+            <div class="itemDetails">
+                <h2>$title</h2>
+                <p>Time: $timeLeft</p>
+                <p>Current Bid: $currentBid</p>
             </div>
-            <button class=\"bidButton\">Bid</button>
+            <a href="../../website/frontend/itemdetails/itemdetails.html"><button class="bidButton">Bid</button></a>
         </div>
-    ";
+    HTML;
+
     echo $element;
 }
+
