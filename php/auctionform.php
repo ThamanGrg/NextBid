5edbdad985e5b8bf4d91848961857c783d482568
ThamanGrg <gurungthaman246@gmail.com>
2025-01-22 10:55:05 +0545

    connection with database and submission of data to database

--- php/auctionform.php
+++ php/auctionform.php
@@ -1,22 +1,27 @@
 <?php
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+include_once('../php/connection.php');
+
+if (isset($_POST['submit'])) {
     $itemName = $_POST['itemName'];
     $initialPrice = $_POST['initialPrice'];
     $maximumPrice = $_POST['maximumPrice'];
+    $postDate = date("y/m/d");
     $startDate = $_POST['startDate'];
     $startTime = $_POST['startTime'];
     $endDate = $_POST['endDate'];
     $endTime = $_POST['endTime'];
     $itemDescription = $_POST['itemDescription'];
 
-    echo  $itemName;
-    echo $initialPrice;
-    echo $maximumPrice;
-    echo $startDate;
-    echo $startTime;
-    echo $endDate;
-    echo $endTime;
-    echo $itemDescription;
-} 
-?>
+    $image_name = $_FILES['itemImage']['name'];
+    $tmp = explode('.', $image_name);
+    $newFileName = round(microtime(true)) . '.' . end($tmp);
+    $uploadPath = "../uploads/" . $newFileName;
+    move_uploaded_file($_FILES['itemImage']['tmp_name'], $uploadPath);
 
+    $sql = "INSERT INTO products (item_name, initial_price, maximum_price, post_date, starting_date, startTime, ending_date, endTime, item_description, item_image) VALUES ('$itemName', '$initialPrice', '$maximumPrice', '$postDate', '$startDate', '$startTime', '$endDate', '$endTime', '$itemDescription', '$uploadPath')";
+
+    $data = mysqli_query($conn, $sql);
+    if ($data){
+        header('location:../website/frontend/Create/create.php');
+    }
+}
