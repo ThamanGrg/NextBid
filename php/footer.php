55ac1ef601cb7f1e66ee4660ceac1be8826d764b
ThamanGrg <gurungthaman246@gmail.com>
2025-01-28 14:04:01 +0545

    Register For Bid

--- php/footer.php
+++ php/footer.php
@@ -29,4 +29,67 @@
             <a href="#"><img src="../../assets/icons/twitter.png" alt=""></a>
         </div>
     </div>
+    <style>
+        footer {
+            width: 100%;
+            height: 500px;
+            margin-top: 10%;
+            background-color: #F8F8FA;
+            border-radius: 20px;
+            display: flex;
+            justify-content: center;
+            position: relative;
+        }
+
+        .socialLinks {
+            background-color: #758BFD;
+            width: 80%;
+            height: 50px;
+            position: absolute;
+            bottom: 0;
+            left: 50%;
+            transform: translate(-50%, 50%);
+            border-top-left-radius: 20px;
+            border-top-right-radius: 20px;
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+        }
+
+        .socialLinks img {
+            width: 30px;
+            padding-left: 15px;
+            padding-top: 5px;
+        }
+
+        .footerMenus {
+            display: flex;
+            justify-content: center;
+            margin-top: 100px;
+        }
+
+        .footerMenus a {
+            font-size: 18px;
+            margin: 2px 0;
+        }
+
+        .footerMenus div {
+            display: flex;
+            flex-direction: column;
+            margin: 0 100px;
+            top: 0;
+        }
+
+        .footerMenus h1 {
+            font-size: 50px;
+            margin-bottom: 20px;
+        }
+
+        .footerMenus hr {
+            transform: rotate(0deg);
+            transform-origin: center;
+            height: 300px;
+            background-color: #D9D9D9;
+        }
+    </style>
 </footer>
